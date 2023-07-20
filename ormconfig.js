aa2bc6901c861a2ad12836dac2e0a821b5237024
flygalmaegi <galmaegifly@gmail.com>
2023-07-20 18:02:00 +0900

    prepare deploy

---
+++ ormconfig.js
@@ -0,0 +1,36 @@
+var dbConfig = {
+  synchronize: false,
+  migrations: ['migrations/*.js'],
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+    });
+    break;
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  default:
+    throw new Error('unkown environment');
+}
+
+module.exports = dbConfig;
